906dacbbbcfdbc38dec6b5bfc1f1781b46607d58
zhangxin <zhangxin@gutou.com>
2019-09-04 14:51:18 +0800

    test

--- main.go
+++ main.go
@@ -32,11 +32,11 @@ func main() {
 	//v1.3.05
 	quit := make(chan os.Signal)
 	signal.Notify(quit, os.Interrupt)
-	<- quit
+	<-quit
 
 	log.Println("Shutdown Server ...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := s.Shutdown(ctx); err != nil {
 		log.Fatal("Server Shutdown:", err)
