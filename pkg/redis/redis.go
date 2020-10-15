6b035b34b92ceaea4998cb5845cccfc7d1be1cb9
zhangxin <vince@cqgg.local>
2020-10-15 14:49:21 +0800

    add redis

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -1,11 +1,21 @@
 package redis
 
-import "github.com/go-redis/redis"
+import (
+	"go-blog/pkg/setting"
+	"log"
 
-func init()  {
+	"github.com/go-redis/redis"
+)
+
+func init() {
+	sec, err := setting.Cfg.GetSection("redis")
+	if err != nil {
+		log.Fatal(2, "Fail to get section 'database': %v", err)
+	}
+
 	redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr:     sec.Key("REDIS_HOST").String(),
+		Password: sec.Key("REDIS_AUTH").String(), // no password set
+		DB:       sec.Key("REDIS_DB").MustInt(1), // use default DB
 	})
 }
